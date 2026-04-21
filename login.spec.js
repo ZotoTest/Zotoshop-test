1d05f61942373ad25b2b8fc09e9ba8b24dde379b
Fodé CISSE <contact+fode@zotomatise.com>
2026-04-21 17:19:06 +0200

    Modification specification login

---
+++ login.spec.js
@@ -0,0 +1,22 @@
+describe("Login ZotoShop", () => {
+  beforeEach(() => {
+    cy.visit("/account/login");
+  });
+
+  it("should display login form", () => {
+    cy.log("TODO: implémenter ce test"); 
+  });
+
+  it("should login with valid credentials", () => {
+    cy.log("TODO: implémenter ce test");
+    cy.log("TODO: implémenter ce test2");
+  });
+
+  it("should reject invalid credentials", () => {
+    cy.log("TODO: implémenter ce test");
+  });
+  
+  it("should reject invalid credentials", () => {
+    cy.log("TODO: implémenter ce test");
+  });
+});
