9843e57d5c1a5e3262faf3ad916fdfe1f314c9b6
Fodé CISSE <contact+fode@zotomatise.com>
2026-04-23 16:46:54 +0200

    add scenario invalid credentials

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -18,7 +18,8 @@ describe("Login ZotoShop", () => {
   it("should reject invalid credentials 2", () => {
 cy.log("TODO: implémenter ce test");
     cy.log("TODO: implémenter ce test2");git 
-    cy.log("TODO: implémenter ce test3");
+    cy.log("TODO: implémenter ce test3")
+    cy.log("TODO: implémenter ce test3")
   
 
   });
