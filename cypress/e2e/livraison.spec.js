20bf4abaad5625a16d60630b5dab298222b15686
Fodé CISSE <contact+fode@zotomatise.com>
2026-04-23 16:32:01 +0200

    création spec livraison

---
+++ cypress/e2e/livraison.spec.js
@@ -0,0 +1,17 @@
+describe("Panier ZotoShop", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("should add product to cart", () => {
+    cy.log("TODO: implémenter ce test");
+  });
+
+  it("should update product quantity in cart", () => {
+    cy.log("TODO: implémenter ce test");
+  });
+
+  it("should remove product from cart", () => {
+    cy.log("TODO: implémenter ce test");
+  });
+});
